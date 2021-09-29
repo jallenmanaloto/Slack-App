98de121b2c3296e4b1c9ab3bf876ab7cf6c31d90
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2021-09-29 23:59:24 +0800

    integrated initial data for Context

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useContext, useState } from 'react';
 import './App.css';
 import Registration from './Components/Registration/Registration'; 
 import Login from './Components/Login/Login';
@@ -7,11 +8,11 @@ import HomeChannel from './Components/Channel/HomeChannel'
 import { ContextAPI } from './Components/Context/ContextAPi';
 
 function App() {
-  const data = {}
+  const [data, setData] = useState();
 
   return (
     <div className="App">
-      <ContextAPI.Provider value={data} >
+      <ContextAPI.Provider value={{data, setData}} >
           {/* <Registration/>
         <Login/> */}
         <Main />
